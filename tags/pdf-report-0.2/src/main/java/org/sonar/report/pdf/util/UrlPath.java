b3acb595c9ca1cd13db71a0e335d13fa2ab7d675
amuniz <amuniz@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-05-27 18:16:07 +0000

    [maven-scm] copy for tag pdf-report-0.2

---
+++ tags/pdf-report-0.2/src/main/java/org/sonar/report/pdf/util/UrlPath.java
@@ -0,0 +1,35 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2009 GMV-SGI
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.report.pdf.util;
+
+public class UrlPath {
+  
+  // First level
+  public static final String RESOURCES = "/api/resources?resource=";
+  
+  // Second level
+  public static final String PARENT_PROJECT = "&depth=0&format=xml";
+  public static final String CHILD_PROJECTS = "&depth=1&format=xml";
+  public static final String CATEGORIES_VIOLATIONS = "&metrics="+ MetricKeys.MANDATORY_VIOLATIONS +"&filter_rules_cats=false&format=xml";
+  public static final String MOST_VIOLATED_RULES = "&metrics="+ MetricKeys.MANDATORY_VIOLATIONS +"&limit=5&filter_rules=false&filter_rules_cats=true&format=xml";
+  public static final String MOST_VIOLATED_FILES = "&metrics="+ MetricKeys.MANDATORY_VIOLATIONS +"&scopes=FIL&depth=-1&limit=5&format=xml";
+  public static final String MOST_COMPLEX_FILES = "&metrics="+ MetricKeys.COMPLEXITY +"&scopes=FIL&depth=-1&limit=5&format=xml";
+  public static final String MOST_DUPLICATED_FILES = "&metrics="+ MetricKeys.DUPLICATED_LINES +"&scopes=FIL&depth=-1&limit=5&format=xml";
+
+}
---
+++ tags/pdf-report-0.2/src/test/java/org/sonar/report/pdf/test/ReporterTest.java
@@ -0,0 +1,47 @@
+package org.sonar.report.pdf.test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.sonar.report.pdf.ExecutivePDFReporter;
+import org.sonar.report.pdf.PDFReporter;
+import org.testng.annotations.Test;
+
+import com.lowagie.text.DocumentException;
+import java.net.URL;
+
+public class ReporterTest {
+
+  /**
+   * Build a PDF report for the Sonar project on "sonar.base.url" instance of Sonar. The property "sonar.base.url" is
+   * set in report.properties, this file will be provided by the artifact consumer.
+   * 
+   * The key of the project is not place in properties, this is provided in execution time.
+   */
+  @Test(enabled = true, groups = { "report" }, dependsOnGroups = { "metrics" })
+  public void getReportTest() throws DocumentException, IOException, org.dom4j.DocumentException {
+    URL resource = this.getClass().getClassLoader().getResource("report.properties");
+    Properties config = new Properties();
+    config.load(resource.openStream());
+    config.setProperty("sonar.base.url", "http://nemo.sonar.codehaus.org");
+    
+    URL resourceText = this.getClass().getClassLoader().getResource("report-texts-en.properties");
+    Properties configText = new Properties();
+    configText.load(resourceText.openStream());
+
+    PDFReporter reporter = new ExecutivePDFReporter(this.getClass().getResource("/sonar-large.png"),
+        "org.codehaus.sonar:sonar", "http://nemo.sonar.codehaus.org", config, configText);
+
+    ByteArrayOutputStream baos = reporter.getReport();
+    FileOutputStream fos = null;
+
+    fos = new FileOutputStream("target/testReport.pdf");
+
+    baos.writeTo(fos);
+    fos.flush();
+    fos.close();
+
+  }
+}
