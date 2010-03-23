87920bf12a5bfadea57c960ea06bcc5bce6160f1
ogaudin <ogaudin@d25a0af3-c461-0410-a42d-853a01480fe4>
2010-03-23 21:30:18 +0000

    [maven-release-plugin]  copy for tag sonar-sigmm-plugin-0.3.1

--- profiler/sonar-profiler-plugin/src/main/java/org/sonar/plugins/profiler/viewer/client/Metrics.java
+++
@@ -1,9 +0,0 @@
-package org.sonar.plugins.profiler.viewer.client;
-
-/**
- * @author Evgeny Mandrikov
- */
-public interface Metrics {
-  String CPU_HOTSPOTS_DATA = "profiler_cpu_hotspots";
-  String MEMORY_HOTSPOTS_DATA = "profiler_memory_hotspots";
-}
