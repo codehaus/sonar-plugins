df5c06d6587f821b40a752e022373597c77c858f
godin <godin@d25a0af3-c461-0410-a42d-853a01480fe4>
2010-04-12 20:26:56 +0000

    [maven-release-plugin]  copy for tag scm-activity-1.1

--- sonarj/src/main/scripts/start.sh
+++
@@ -1,7 +0,0 @@
-sonardir=/opt/sonar-2.0.1
-arch=macosx-universal-64
-mvn clean install
-rm $sonardir/extensions/plugins/sonar-sonarj-plugin*
-cp target/sonar-sonarj-plugin*.jar $sonardir/extensions/plugins
-$sonardir/bin/$arch/sonar.sh start
-
