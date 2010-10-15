e3c894e86bb527273aa571934d27e429c75a0417
godin <godin@d25a0af3-c461-0410-a42d-853a01480fe4>
2010-10-15 23:19:47 +0000

    [maven-release-plugin]  copy for tag sonar-jacoco-plugin-0.2

---
+++ tags/sonar-jacoco-plugin-0.2/src/main/java/org/sonar/plugins/jacoco/itcoverage/JaCoCoItSensor.java
@@ -0,0 +1,150 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2010 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.jacoco.itcoverage;
+
+import org.apache.commons.lang.StringUtils;
+import org.jacoco.core.analysis.CoverageBuilder;
+import org.jacoco.core.analysis.ICoverageNode;
+import org.jacoco.core.analysis.ILines;
+import org.jacoco.core.analysis.SourceFileCoverage;
+import org.jacoco.core.data.ExecutionDataReader;
+import org.jacoco.core.data.ExecutionDataStore;
+import org.jacoco.core.data.SessionInfoStore;
+import org.jacoco.core.instr.Analyzer;
+import org.sonar.api.batch.AbstractCoverageExtension;
+import org.sonar.api.batch.Sensor;
+import org.sonar.api.batch.SensorContext;
+import org.sonar.api.measures.Measure;
+import org.sonar.api.measures.PersistenceMode;
+import org.sonar.api.measures.PropertiesBuilder;
+import org.sonar.api.resources.JavaFile;
+import org.sonar.api.resources.Project;
+import org.sonar.api.utils.SonarException;
+import org.sonar.plugins.jacoco.JaCoCoPlugin;
+import org.sonar.plugins.jacoco.JaCoCoUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Note that this class can't extend {@link AbstractCoverageExtension}, because in this case this extension will be disabled under Sonar
+ * 2.3, if JaCoCo is not defined as the default code coverage plugin.
+ * 
+ * @author Evgeny Mandrikov
+ */
+public class JaCoCoItSensor implements Sensor {
+  private PropertiesBuilder<Integer, Integer> lineHitsBuilder = new PropertiesBuilder<Integer, Integer>();
+
+  public JaCoCoItSensor() {
+  }
+
+  public boolean shouldExecuteOnProject(Project project) {
+    return StringUtils.isNotBlank(project.getConfiguration().getString(JaCoCoPlugin.IT_REPORT_PATH_PROPERTY)) && project.getAnalysisType().isDynamic(true);
+  }
+
+  public void analyse(Project project, SensorContext context) {
+    final File buildOutputDir = project.getFileSystem().getBuildOutputDir();
+    if ( !buildOutputDir.exists()) {
+      JaCoCoUtils.LOG.info("Can't find build output directory: {}. Skipping JaCoCo analysis.", buildOutputDir);
+      return;
+    }
+    String path = getReportPath(project);
+    File jacocoExecutionData = project.getFileSystem().resolvePath(path);
+    try {
+      readExecutionData(jacocoExecutionData, buildOutputDir, context);
+    } catch (IOException e) {
+      throw new SonarException(e);
+    }
+  }
+
+  public void readExecutionData(File jacocoExecutionData, File buildOutputDir, SensorContext context) throws IOException {
+    SessionInfoStore sessionInfoStore = new SessionInfoStore();
+    ExecutionDataStore executionDataStore = new ExecutionDataStore();
+
+    if (jacocoExecutionData == null || !jacocoExecutionData.exists() || !jacocoExecutionData.isFile()) {
+      JaCoCoUtils.LOG.info("Can't find JaCoCo execution data : {}. Project coverage is set to 0%.", jacocoExecutionData);
+    } else {
+      JaCoCoUtils.LOG.info("Analysing {}", jacocoExecutionData);
+      ExecutionDataReader reader = new ExecutionDataReader(new FileInputStream(jacocoExecutionData));
+      reader.setSessionInfoVisitor(sessionInfoStore);
+      reader.setExecutionDataVisitor(executionDataStore);
+      reader.read();
+    }
+
+    CoverageBuilder coverageBuilder = new CoverageBuilder(executionDataStore);
+
+    Analyzer analyzer = new Analyzer(coverageBuilder);
+    analyzer.analyzeAll(buildOutputDir);
+
+    for (SourceFileCoverage coverage : coverageBuilder.getSourceFiles()) {
+      String fileName = StringUtils.substringBeforeLast(coverage.getName(), ".");
+      String resourceName = StringUtils.replaceChars(coverage.getPackageName() + "/" + fileName, '/', '.');
+
+      JavaFile resource = new JavaFile(resourceName);
+      analyzeClass(resource, coverage, context);
+    }
+  }
+
+  private void analyzeClass(JavaFile resource, ICoverageNode coverage, SensorContext context) {
+    lineHitsBuilder.clear();
+
+    final ILines lines = coverage.getLines();
+    for (int lineId = lines.getFirstLine(); lineId <= lines.getLastLine(); lineId++) {
+      final int fakeHits;
+      switch (lines.getStatus(lineId)) {
+        case ILines.FULLY_COVERED:
+          fakeHits = 1;
+          break;
+        case ILines.PARTLY_COVERED:
+        case ILines.NOT_COVERED:
+          fakeHits = 0;
+          break;
+        case ILines.NO_CODE:
+          continue;
+        default:
+          JaCoCoUtils.LOG.warn("Unknown status for line {} in {}", lineId, resource);
+          continue;
+      }
+      lineHitsBuilder.add(lineId, fakeHits);
+    }
+
+    saveMeasures(context, resource, lines, lineHitsBuilder.buildData());
+  }
+
+  protected String getReportPath(Project project) {
+    return project.getConfiguration().getString(JaCoCoPlugin.IT_REPORT_PATH_PROPERTY, JaCoCoPlugin.IT_REPORT_PATH_DEFAULT_VALUE);
+  }
+
+  protected void saveMeasures(SensorContext context, JavaFile resource, ILines lines, String lineHitsData) {
+    if (context.getResource(resource) != null) {
+      context.saveMeasure(resource, JaCoCoItMetrics.IT_LINES_TO_COVER, (double) lines.getTotalCount());
+      context.saveMeasure(resource, JaCoCoItMetrics.IT_UNCOVERED_LINES, (double) lines.getMissedCount());
+      Measure lineHits = new Measure(JaCoCoItMetrics.IT_COVERAGE_LINE_HITS_DATA).setData(lineHitsData);
+      context.saveMeasure(resource, lineHits.setPersistenceMode(PersistenceMode.DATABASE));
+    }
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName();
+  }
+}
---
+++ tags/sonar-jacoco-plugin-0.2/src/test/java/org/sonar/plugins/jacoco/JaCoCoSensorTest.java
@@ -0,0 +1,107 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2010 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.jacoco;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Matchers.doubleThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.Plugins;
+import org.sonar.api.batch.SensorContext;
+import org.sonar.api.measures.CoreMetrics;
+import org.sonar.api.measures.Measure;
+import org.sonar.api.resources.JavaFile;
+import org.sonar.api.resources.Project;
+import org.sonar.api.resources.Resource;
+import org.sonar.api.test.IsMeasure;
+
+import java.io.File;
+
+/**
+ * @author Evgeny Mandrikov
+ */
+public class JaCoCoSensorTest {
+  private JaCoCoSensor sensor;
+
+  @Before
+  public void setUp() {
+    sensor = new JaCoCoSensor(mock(Plugins.class), mock(JaCoCoMavenPluginHandler.class));
+  }
+
+  @Test
+  public void testSensorDefinition() {
+    assertThat(sensor.toString(), is("JaCoCoSensor"));
+  }
+
+  @Test
+  public void testGetMavenPluginHandler() {
+    Project project = mock(Project.class);
+    when(project.getAnalysisType())
+        .thenReturn(Project.AnalysisType.REUSE_REPORTS)
+        .thenReturn(Project.AnalysisType.DYNAMIC);
+
+    assertThat(sensor.getMavenPluginHandler(project), nullValue());
+    assertThat(sensor.getMavenPluginHandler(project), instanceOf(JaCoCoMavenPluginHandler.class));
+  }
+
+  @Test
+  public void testReadExecutionData() throws Exception {
+    File jacocoExecutionData = new File(getClass().getResource("/org/sonar/plugins/jacoco/JaCoCoSensorTest/jacoco.exec").getFile());
+    File buildOutputDir = jacocoExecutionData.getParentFile();
+    SensorContext context = mock(SensorContext.class);
+
+    final JavaFile resource = new JavaFile("org.sonar.plugins.jacoco.tests.Hello");
+    when(context.getResource(any(Resource.class))).thenReturn(resource);
+
+    sensor.readExecutionData(jacocoExecutionData, buildOutputDir, context);
+
+    verify(context).getResource(eq(resource));
+    verify(context).saveMeasure(eq(resource), eq(CoreMetrics.LINES_TO_COVER), doubleThat(greaterThan(0d)));
+    verify(context).saveMeasure(eq(resource), eq(CoreMetrics.UNCOVERED_LINES), doubleThat(greaterThan(0d)));
+    verify(context).saveMeasure(eq(resource), argThat(new IsMeasure(CoreMetrics.COVERAGE_LINE_HITS_DATA)));
+    verifyNoMoreInteractions(context);
+  }
+
+  @Test
+  public void doNotSaveMeasureOnResourceWhichDoesntExistInTheContext() throws Exception {
+    File jacocoExecutionData = new File(getClass().getResource("/org/sonar/plugins/jacoco/JaCoCoSensorTest/jacoco.exec").getFile());
+    File buildOutputDir = jacocoExecutionData.getParentFile();
+    SensorContext context = mock(SensorContext.class);
+    when(context.getResource(any(Resource.class))).thenReturn(null);
+
+    sensor.readExecutionData(jacocoExecutionData, buildOutputDir, context);
+
+    verify(context, never()).saveMeasure(any(Resource.class), any(Measure.class));
+  }
+}
