4271b96cda388716ca875b7ece5758d10aae6be0
simon.brandhof <simon.brandhof@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-02-18 13:47:32 +0000

    [maven-release-plugin]  copy for tag sonar-ant-task-1.0

--- webscanner/tests/validate-html.bat
+++
@@ -1,8 +0,0 @@
-
-rem run this script from webscanner folder
-
-set SONAR_FLAGS=-Dsonar.host.url=http://localhost:9000 -Dsonar.jdbc.url=jdbc:postgresql://localhost/sonar2.5 -Dsonar.jdbc.driver=org.postgresql.Driver
-set DEBUG= 
-
-call %mvncommand% -f tests/html-pom.xml %SONAR_FLAGS% %DEBUG% > sonar-html.log
-call %mvncommand% -f tests/xhtml-pom.xml %SONAR_FLAGS% %DEBUG% > sonar-xml.log
