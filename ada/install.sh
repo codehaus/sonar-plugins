95c166b47b190cd4e349db6fbae550858f029bf5
simon.brandhof <simon.brandhof@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-05-24 09:35:35 +0000

    [maven-release-plugin]  copy for tag sonar-clover-plugin-2.8

--- ada/install.sh
+++
@@ -1,5 +0,0 @@
-SONAR_HOME=~/sonar/sonar-2.7/
-
-mvn clean install
-cp target/*.jar $SONAR_HOME/extensions/plugins
-
--- webscanner/tests/validate-html.bat
+++
@@ -1,8 +0,0 @@
-
-rem run this script from webscanner folder
-
-set SONAR_FLAGS=
-set DEBUG= 
-
-call %mvncommand% -f tests/html-pom.xml %SONAR_FLAGS% %DEBUG% > sonar-html.log
-call %mvncommand% -f tests/xhtml-pom.xml %SONAR_FLAGS% %DEBUG% > sonar-xml.log
