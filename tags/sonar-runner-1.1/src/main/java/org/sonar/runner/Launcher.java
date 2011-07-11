8191c01aac9c078aea6260603173d02797595b45
simon.brandhof <simon.brandhof@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-07-11 10:29:10 +0000

    [maven-release-plugin]  copy for tag sonar-runner-1.1

---
+++ tags/sonar-runner-1.1/src/main/java/org/sonar/runner/Launcher.java
@@ -0,0 +1,112 @@
+/*
+ * Sonar Standalone Runner
+ * Copyright (C) 2011 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.runner;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Properties;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import org.apache.commons.configuration.*;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.LoggerFactory;
+import org.sonar.api.utils.SonarException;
+import org.sonar.batch.Batch;
+import org.sonar.batch.bootstrapper.EnvironmentInformation;
+import org.sonar.batch.bootstrapper.ProjectDefinition;
+import org.sonar.batch.bootstrapper.Reactor;
+
+public class Launcher {
+
+  private Runner runner;
+
+  public Launcher(Runner runner) {
+    this.runner = runner;
+  }
+
+  /**
+   * This method invoked from {@link Main}. Do not rename it.
+   */
+  public void execute() {
+    initLogging();
+    executeBatch();
+  }
+
+  private void executeBatch() {
+    ProjectDefinition project = defineProject();
+    Reactor reactor = new Reactor(project);
+    Batch batch = new Batch(getInitialConfiguration(project), new EnvironmentInformation("Runner", runner.getRunnerVersion()), reactor);
+    batch.execute();
+  }
+
+  private void initLogging() {
+    LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+    JoranConfigurator jc = new JoranConfigurator();
+    jc.setContext(context);
+    context.reset();
+    InputStream input = Batch.class.getResourceAsStream("/org/sonar/batch/logback.xml");
+    System.setProperty("ROOT_LOGGER_LEVEL", runner.isDebug() ? "DEBUG" : "INFO");
+    try {
+      jc.doConfigure(input);
+
+    } catch (JoranException e) {
+      throw new SonarException("can not initialize logging", e);
+
+    } finally {
+      IOUtils.closeQuietly(input);
+    }
+  }
+
+  private ProjectDefinition defineProject() {
+    File baseDir = runner.getProjectDir();
+    Properties properties = runner.getProperties();
+    ProjectDefinition definition = new ProjectDefinition(baseDir, runner.getWorkDir(), properties);
+    for (String dir : getList(properties, "sources")) {
+      definition.addSourceDir(dir);
+    }
+    for (String dir : getList(properties, "tests")) {
+      definition.addTestDir(dir);
+    }
+    for (String dir : getList(properties, "binaries")) {
+      definition.addBinaryDir(dir);
+    }
+    for (String file : getList(properties, "libraries")) {
+      definition.addLibrary(file);
+    }
+    return definition;
+  }
+
+  private String[] getList(Properties properties, String key) {
+    return StringUtils.split(properties.getProperty(key, ""), ',');
+  }
+
+  private Configuration getInitialConfiguration(ProjectDefinition project) {
+    CompositeConfiguration configuration = new CompositeConfiguration();
+    configuration.addConfiguration(new SystemConfiguration());
+    configuration.addConfiguration(new EnvironmentConfiguration());
+    configuration.addConfiguration(new MapConfiguration(project.getProperties()));
+    return configuration;
+  }
+
+}
---
+++ tags/sonar-runner-1.1/src/test/java/org/sonar/runner/MainTest.java
@@ -0,0 +1,102 @@
+/*
+ * Sonar Standalone Runner
+ * Copyright (C) 2011 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.runner;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MainTest {
+
+  @Test
+  public void shouldParseEmptyArguments() {
+    Properties props = Main.parseArguments(new String[]{});
+    assertThat(props.isEmpty(), is(true));
+  }
+
+  @Test
+  public void shouldParseArguments() {
+    Properties props = Main.parseArguments(new String[]{"-D", "foo=bar", "--define", "hello=world"});
+    assertThat(props.size(), is(2));
+    assertThat(props.getProperty("foo"), is("bar"));
+    assertThat(props.getProperty("hello"), is("world"));
+  }
+
+  @Test
+  public void shouldEnableDebugMode() {
+    Properties props = Main.parseArguments(new String[]{"-X"});
+    assertThat(props.getProperty(Runner.DEBUG_MODE), is("true"));
+  }
+
+  @Test
+  public void shouldDisableDebugModeByDefault() {
+    Properties props = Main.parseArguments(new String[]{});
+    assertThat(props.getProperty(Runner.DEBUG_MODE), nullValue());
+  }
+
+  @Test
+  public void shouldLoadRunnerSettingsByHome() throws Exception {
+    File home = new File(getClass().getResource("/org/sonar/runner/MainTest/shouldLoadRunnerSettingsByHome/").toURI());
+    Properties args = new Properties();
+    args.setProperty("runner.home", home.getCanonicalPath());
+
+    Properties props = Main.loadRunnerProperties(args);
+
+    assertThat(props.getProperty("sonar.host.url"), is("http://moon/sonar"));
+  }
+
+  @Test
+  public void shouldNotFailIfNoHome() throws Exception {
+    Properties args = new Properties();
+    Properties props = Main.loadRunnerProperties(args);
+
+    assertThat(props.isEmpty(), is(true));
+  }
+
+  @Test
+  public void shouldLoadRunnerSettingsByDirectPath() throws Exception {
+    File settings = new File(getClass().getResource("/org/sonar/runner/MainTest/shouldLoadRunnerSettingsByDirectPath/other-conf.properties").toURI());
+    Properties args = new Properties();
+    args.setProperty("runner.settings", settings.getCanonicalPath());
+    Properties props = Main.loadRunnerProperties(args);
+
+    assertThat(props.getProperty("sonar.host.url"), is("http://other/sonar"));
+  }
+
+  @Test
+  public void shouldLoadCompleteConfiguration() throws Exception {
+    File runnerHome = new File(getClass().getResource("/org/sonar/runner/MainTest/shouldLoadCompleteConfiguration/runner").toURI());
+    File projectHome = new File(getClass().getResource("/org/sonar/runner/MainTest/shouldLoadCompleteConfiguration/project").toURI());
+    Properties props = Main.loadProperties(new String[]{
+        "-D", "runner.home=" + runnerHome.getCanonicalPath(),
+        "-D", "project.home=" + projectHome.getCanonicalPath()
+    });
+
+    assertThat(props.getProperty("project.prop"), is("foo"));
+    assertThat(props.getProperty("overridden.prop"), is("project scope"));
+    assertThat(props.getProperty("global.prop"), is("jdbc:mysql:localhost/sonar"));
+  }
+}
