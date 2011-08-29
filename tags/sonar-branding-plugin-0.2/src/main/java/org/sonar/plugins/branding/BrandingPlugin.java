e6095e1bd7c36cc83de2a3359cf0731db373cee4
ogaudin <ogaudin@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-08-29 11:33:24 +0000

    [maven-release-plugin]  copy for tag sonar-branding-plugin-0.2

---
+++ tags/sonar-branding-plugin-0.2/src/main/java/org/sonar/plugins/branding/BrandingPlugin.java
@@ -0,0 +1,45 @@
+/*
+ * Sonar Branding Plugin
+ * Copyright (C) 2011 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.branding;
+
+import java.util.Arrays;
+import java.util.List;
+import org.sonar.api.Properties;
+import org.sonar.api.Property;
+import org.sonar.api.SonarPlugin;
+
+@Properties({ 
+    @Property(key = BrandingPlugin.IMAGE_PROPERTY,
+    name = "Image URL",
+    description = "Example : http://www.codehaus.org/codehaus-small.gif"),
+    @Property(key = BrandingPlugin.LOGO_LOCATION_PROPERTY,
+            name = "Logo location in Sonar UI",
+            description = "Possible values: TOP, MENU", defaultValue="TOP")})
+public class BrandingPlugin extends SonarPlugin {
+
+  public static final String IMAGE_PROPERTY = "sonar.branding.image";
+  public static final String LOGO_LOCATION_PROPERTY = "sonar.branding.logo.location";
+
+  public List getExtensions() {
+    return Arrays.asList(LogoFooter.class, ProjectLogoWidget.class);
+  }
+
+}
