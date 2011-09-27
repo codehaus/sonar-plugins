c68838bed82c062c4071d064cd68076ce8170ca6
fabemn <fabemn@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-09-27 07:27:18 +0000

    Temporary test project just for test purposes

---
+++ test/src/main/java/Toto.java
@@ -0,0 +1,13 @@
+public class Toto {
+  
+  public static void main(String[] args) {
+    
+  }
+  
+  public void foo() {
+    System.out.println("Bla");
+    System.out.println("Bla");
+    System.out.println("Bla");	
+  }
+  
+}
