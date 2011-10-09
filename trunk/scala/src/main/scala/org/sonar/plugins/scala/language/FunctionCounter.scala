ad261866737c3bc1ab995acd865e70a638a58369
fmueller <fmueller@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-10-09 12:31:03 +0000

    Refactored and cleaned up code

--- trunk/scala/src/main/scala/org/sonar/plugins/scala/language/FunctionCounter.scala
+++ trunk/scala/src/main/scala/org/sonar/plugins/scala/language/FunctionCounter.scala
@@ -37,6 +37,41 @@ object FunctionCounter {
   // TODO improve counting functions
   def countFunctions(source: String) = {
 
+    def isEmptyConstructor(constructor: DefDef) = {
+      if (constructor.name.startsWith(nme.CONSTRUCTOR) ||
+            constructor.name.startsWith(nme.MIXIN_CONSTRUCTOR)) {
+
+        constructor.rhs match {
+
+          case Block(stats, expr) =>
+            if (stats.size == 0) {
+              true
+            } else {
+              stats.size == 1 &&
+                  (stats(0).toString().startsWith("super." + nme.CONSTRUCTOR) ||
+                      stats(0).toString().startsWith("super." + nme.MIXIN_CONSTRUCTOR)) &&
+                  isEmptyBlock(expr)
+            }
+
+          case _ =>
+            constructor.isEmpty
+        }
+      } else {
+        false
+      }
+    }
+
+    def isEmptyBlock(block: Tree) = block match {
+      case literal: Literal =>
+        val isEmptyConstant = literal.value match {
+          case Constant(value) => value.toString().equals("()")
+          case _ => false
+        }
+        literal.isEmpty || isEmptyConstant
+
+      case _ => block.isEmpty
+    }
+
     def countFunctionTrees(tree: Tree, foundFunctions: Int = 0) : Int = tree match {
 
       // recursive descent until found a syntax tree with countable functions
@@ -89,13 +124,12 @@ object FunctionCounter {
        * Countable function declarations are functions, methods and closures.
        */
 
-      case defDef: DefDef => {
+      case defDef: DefDef =>
         if (isEmptyConstructor(defDef)) {
           countFunctionTrees(defDef.rhs, foundFunctions)
         } else {
           countFunctionTrees(defDef.rhs, foundFunctions + 1)
         }
-      }
 
       case Function(_, body) =>
         countFunctionTrees(body, foundFunctions + 1)
