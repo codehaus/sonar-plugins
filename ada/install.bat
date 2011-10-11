39aae20b291ec17ec7f08db9d132a8d4161e3d64
ogaudin <ogaudin@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-10-11 15:34:14 +0000

    [maven-release-plugin]  copy for tag sonar-ant-task-1.2

--- ada/install.bat
+++
@@ -1,7 +0,0 @@
-
-set PLUGINS_HOME=d:\java\sonar-2.3\extensions\plugins
-set PLUGIN_VERSION=0.1-SNAPSHOT
-
-call mvn clean package -DskipTests=true
-copy target\sonar-ada-plugin-%PLUGIN_VERSION%.jar %PLUGINS_HOME%
-
