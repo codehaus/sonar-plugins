8c32e1f66fe42ca44b07430575b40f506eae9485
dinesh <dinesh@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-11-11 15:47:41 +0000

    SONARPLUGINS-1497 Rule : Functions with no parameters shall be declared with parameter type void

---
+++ c-rules/src/test/resources/checks/functionsWithNoParametersSahllBeDeclaredWithParameterTypeVoid.c
@@ -0,0 +1,24 @@
+void myFunctionWithoutParameters();
+void myFunctionWithoutParametersButWithVoid(void);
+
+void myFunctionWithoutParameters()
+{
+
+}
+
+void myFunctionWithoutParametersButWithVoid(void)
+{
+
+}
+
+void myFunctionKRStyle(a, b)
+  char a;
+  char b;
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
