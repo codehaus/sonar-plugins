4a9a483aa7a8ce1442f35f79bf37e76a1b57797f
dinesh <dinesh@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-11-25 16:22:28 +0000

    C-Rules: Fix IT

--- c-rules/src/main/java/org/sonar/c/checks/CCheckPlugin.java
+++ c-rules/src/main/java/org/sonar/c/checks/CCheckPlugin.java
@@ -20,55 +20,13 @@
 
 package org.sonar.c.checks;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.sonar.api.SonarPlugin;
 
-import com.sonarsource.c.plugin.CCheck;
-
 public class CCheckPlugin extends SonarPlugin {
 
   public List getExtensions() {
-    List<Class<? extends CCheck>> extensions = new ArrayList<Class<? extends CCheck>>();
-
-    extensions.add(ParsingErrorCheck.class);
-    extensions.add(FunctionComplexityCheck.class);
-    extensions.add(EmptyBlockCheck.class);
-    extensions.add(SwitchStatementWithoutDefaultCheck.class);
-    extensions.add(BooleanExpressionComplexityCheck.class);
-    extensions.add(NestedIfDepthCheck.class);
-    extensions.add(FunctionLocCheck.class);
-    extensions.add(FileLocCheck.class);
-    extensions.add(IfStatementWithoutBracesCheck.class);
-    extensions.add(WhileLoopWithoutBracesCheck.class);
-    extensions.add(ForLoopWithoutBracesCheck.class);
-    extensions.add(CollapsibleIfStatementsCheck.class);
-    extensions.add(ExcessiveParameterListCheck.class);
-    extensions.add(FunctionNameCheck.class);
-    extensions.add(FileNameCheck.class);
-    extensions.add(BreakCheck.class);
-    extensions.add(ContinueCheck.class);
-    extensions.add(GotoCheck.class);
-    extensions.add(CommaOperatorShallNotBeUsedCheck.class);
-    extensions.add(FunctionsWithNoParametersShallBeDeclaredWithParameterTypeVoidCheck.class);
-    extensions.add(FunctionsShallNotBeDefinedWithVariableNumberOfArgumentsCheck.class);
-    extensions.add(IfElseConstructsShallBeTerminatedWithAnElseCheck.class);
-    extensions.add(TrigraphsShallNotBeUsedCheck.class);
-    extensions.add(OctalConstantsAndEscapesShallNotBeUsedCheck.class);
-    extensions.add(NamesShallBeGivenForAllParametersInFunctionPrototypeCheck.class);
-    extensions.add(AnUnconditionalBreakStatementShallTerminateEveryNonEmptyCaseClauseOfSwitchCheck.class);
-    extensions.add(SwitchStatementsWithoutAnyCaseShallBeRefactoredCheck.class);
-    extensions.add(SwitchWithoutBracesCheck.class);
-    extensions.add(MixIncrementAndDecrementWithOtherOperatorsCheck.class);
-    extensions.add(HiddenIdentifiersCheck.class);
-    extensions.add(LineLengthCheck.class);
-    extensions.add(C99CommentsCheck.class);
-    extensions.add(ContainsInnerC89CommentsCheck.class);
-    extensions.add(ContainsInnerC99CommentsCheck.class);
-    extensions.add(IdentifierTooLongCheck.class);
-    extensions.add(CommentedCodeCheck.class);
-
-    return extensions;
+    return CheckList.getChecks();
   }
 }
