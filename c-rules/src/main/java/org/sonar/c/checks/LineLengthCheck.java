0748a89666c1c4e52c55352a3f6ba916e7877100
dinesh <dinesh@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-11-25 10:18:03 +0000

    SONARPLUGINS-730 Rule to detect long lines

---
+++ c-rules/src/main/java/org/sonar/c/checks/LineLengthCheck.java
@@ -0,0 +1,56 @@
+/*
+ * Sonar C-Rules Plugin
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.c.checks;
+
+import org.sonar.check.BelongsToProfile;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.check.RuleProperty;
+
+import com.sonar.sslr.api.AstAndTokenVisitor;
+import com.sonar.sslr.api.AstNode;
+import com.sonar.sslr.api.Token;
+import com.sonarsource.c.plugin.CCheck;
+
+@Rule(key = "C.LineLength", name = "The comma operator shall not be used.",
+    priority = Priority.MAJOR, description = "<p>The comma operator shall not be used.</p>")
+@BelongsToProfile(title = CChecksConstants.SONAR_C_WAY_PROFILE_KEY, priority = Priority.MAJOR)
+public class LineLengthCheck extends CCheck implements AstAndTokenVisitor {
+
+  private final static int DEFAULT_MAXIMUM_LINE_LENHGTH = 80;
+  private int lastIncorrectLine;
+
+  @RuleProperty(key = "maximumLineLength", description = "The maximum authorized line length.", defaultValue = ""
+      + DEFAULT_MAXIMUM_LINE_LENHGTH)
+  public int maximumLineLength = DEFAULT_MAXIMUM_LINE_LENHGTH;
+
+  @Override
+  public void visitFile(AstNode astNode) {
+    lastIncorrectLine = -1;
+  }
+
+  public void visitToken(Token token) {
+    if (lastIncorrectLine != token.getLine() && token.getColumn() + token.getValue().length() > maximumLineLength) {
+      lastIncorrectLine = token.getLine();
+      log("The line length is greater than {0,number,integer} authorized.", token.getLine(), maximumLineLength);
+    }
+  }
+}
---
+++ c-rules/src/test/java/org/sonar/c/checks/LineLengthCheckTest.java
@@ -0,0 +1,60 @@
+/*
+ * Sonar C-Rules Plugin
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.c.checks;
+
+import static org.sonar.c.checks.CheckMatchers.*;
+import static org.sonar.c.checks.CheckUtils.*;
+
+import org.junit.Test;
+
+public class LineLengthCheckTest {
+
+  @Test
+  public void testCheckDefaultParameter() {
+    setCurrentSourceFile(scanFile("/checks/lineLength.c", new LineLengthCheck()));
+
+    assertOnlyOneViolation().atLine(4).withMessage("The line length is greater than 80 authorized.");
+  }
+
+  @Test
+  public void testCheckWithCustomParameter1() {
+    LineLengthCheck check = new LineLengthCheck();
+    check.maximumLineLength = 81;
+
+    setCurrentSourceFile(scanFile("/checks/lineLength.c", check));
+
+    assertNoViolation();
+  }
+
+  @Test
+  public void testCheckWithCustomParameter2() {
+    LineLengthCheck check = new LineLengthCheck();
+    check.maximumLineLength = 79;
+
+    setCurrentSourceFile(scanFile("/checks/lineLength.c", check));
+
+    assertNumberOfViolations(2);
+
+    assertViolation().atLine(3).withMessage("The line length is greater than 79 authorized.");
+    assertViolation().atLine(4);
+  }
+
+}
