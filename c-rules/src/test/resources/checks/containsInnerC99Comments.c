75c63eba196efee05d105e72de3544feb9d05b54
dinesh <dinesh@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-11-25 11:21:54 +0000

    SONARPLUGINS-1524 Rule : The character sequence // shall not be used within a comment.

---
+++ c-rules/src/test/resources/checks/containsInnerC99Comments.c
@@ -0,0 +1,9 @@
+int main(int argc, char* argv[])
+{
+  /* this comment is fine */
+  // this one is also fine
+  /* however not this one // */
+  // nor this one //
+  // this is not covered by the check /*
+  return 0;
+}
