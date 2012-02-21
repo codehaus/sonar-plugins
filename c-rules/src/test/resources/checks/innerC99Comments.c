7c850aa30b7596c014afb2ab4e7dad5672a48a03
cream <cream@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-02-21 09:27:41 +0000

    [maven-release-plugin]  copy for tag sonar-delphi-plugin-0.1

--- c-rules/src/test/resources/checks/innerC99Comments.c
+++
@@ -1,9 +0,0 @@
-int main(int argc, char* argv[])
-{
-  /* this comment is fine */
-  // this one is also fine
-  /* however not this one // */
-  // nor this one //
-  // this is not covered by the check /*
-  return 0;
-}
