04f9a36d07b99a66cc4823ad09e98765bc88c96c
ppapapetrou <ppapapetrou@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-03-18 15:21:45 +0000

    Initial Plugin Structure

---
+++ trunk/thucydides/src/main/java/org/sonar/plugins/thucydides/ThucydidesResultSiteParser.java
@@ -0,0 +1,33 @@
+/*
+ * Sonar Thucydides Plugin
+ * Copyright (C) 2012 OTS SA
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.thucydides;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.api.BatchExtension;
+import org.sonar.api.batch.SensorContext;
+
+public class ThucydidesResultSiteParser implements BatchExtension {
+  private static final Logger LOG = LoggerFactory.getLogger(ThucydidesResultSiteParser.class);
+  public ThucydidesResultSiteParser(SensorContext context) {
+    LOG.debug("Site Parser constructor!");
+  }  
+}
