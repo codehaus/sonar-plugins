7f2cbfd56cea330ccfafd0b8a103b0a94e659622
ganncamp <ganncamp@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-04-25 16:44:23 +0000

    initial commit at correct level :-[

---
+++ shaw-widgets/src/main/java/com/shawinc/sonar/plugin/widget/ManualSeverityWidget.java
@@ -0,0 +1,45 @@
+/*
+ * Sonar Shaw Widgets Plugin
+ * Copyright (C) 2012 Shaw Industries
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package com.shawinc.sonar.plugin.widget;
+
+/**
+ *
+ * @author gcampb2
+ */
+import org.sonar.api.web.*;
+
+@UserRole(UserRole.USER)
+@Description("Shows reviews with a manual severity")
+@WidgetCategory({"Reviews"})
+public class ManualSeverityWidget extends AbstractRubyTemplate implements RubyRailsWidget {
+
+    @Override
+    protected String getTemplatePath() {
+        return ShawWidgetsPlugin.getTemplatePathBase() + "/manual_severity_reviews_widget.html.erb";
+    }
+
+    public String getId() {
+        return "manual_severity_reviews";
+    }
+
+    public String getTitle() {
+        return "Manual Severity Reviews";
+    }
+}
