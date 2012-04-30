1b00f27d4e5a2826b52fdca8f4566a54acaf9e67
ingmarkellner <ingmarkellner@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-04-30 07:33:03 +0000

    [maven-release-plugin]  copy for tag sonar-sonargraph-plugin-3.0.3

---
+++ tags/sonar-sonargraph-plugin-3.0.3/src/test/AlarmClockMain_partialWorkspace2/AlarmClock/src/main/java/com/h2m/alarm/presentation/AlarmToFile.java
@@ -0,0 +1,30 @@
+package com.h2m.alarm.presentation;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.hello2morrow.dda.foundation.common.EncryptUtil;
+
+public final class AlarmToFile extends AlarmHandler
+{
+    @Override
+    public void handleAlarm()
+    {
+      EncryptUtil.encrypt("Test");
+      
+        try
+        {
+            File file = new File("alarm.txt");
+            file.createNewFile();
+            PrintWriter writer = new PrintWriter(file);
+            writer.println("Alarm received");
+            writer.flush();
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
