dff0f3f9eb482f7986f986da9fd7fe860c99aa42
fabemn <fabemn@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-05-14 13:18:25 +0000

    SONAR-2541 Initial commit - plugin may be merged with the sonar-jira-plugin (to be decided soon)
    The plugin is not fully functionnal yet => need to add the possibility to get settings per project on the Java server side

---
+++ trunk/jira-reviews/src/main/java/org/sonar/plugins/reviews/jira/JiraReviewsPlugin.java
@@ -0,0 +1,41 @@
+/*
+ * Sonar JIRA-Reviews Plugin
+ * Copyright (C) 2012 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.reviews.jira;
+
+import com.google.common.collect.Lists;
+import org.sonar.api.Extension;
+import org.sonar.api.SonarPlugin;
+import org.sonar.plugins.reviews.jira.soap.JiraSOAPClient;
+
+import java.util.List;
+
+public final class JiraReviewsPlugin extends SonarPlugin {
+
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  public List<Class<? extends Extension>> getExtensions() {
+    List extensions = Lists.newLinkedList();
+
+    extensions.add(JiraLinkReviewCommand.class);
+    extensions.add(JiraLinkReviewAction.class);
+    extensions.add(JiraSOAPClient.class);
+
+    return extensions;
+  }
+}
