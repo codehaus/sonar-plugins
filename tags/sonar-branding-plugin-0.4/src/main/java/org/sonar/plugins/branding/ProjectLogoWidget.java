58e569be54df9ac2af4c3258e40cc3e4fc4f1391
ppapapetrou <ppapapetrou@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-10-07 12:58:08 +0000

    [maven-release-plugin]  copy for tag sonar-branding-plugin-0.4

---
+++ tags/sonar-branding-plugin-0.4/src/main/java/org/sonar/plugins/branding/ProjectLogoWidget.java
@@ -0,0 +1,43 @@
+/*
+ * Sonar Branding Plugin
+ * Copyright (C) 2011 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.branding;
+
+import org.sonar.api.web.AbstractRubyTemplate;
+import org.sonar.api.web.Description;
+import org.sonar.api.web.RubyRailsWidget;
+
+@Description("Displays project logo")
+public class ProjectLogoWidget extends AbstractRubyTemplate implements RubyRailsWidget {
+
+  public String getId() {
+    return "project-logo";
+  }
+
+  public String getTitle() {
+    return "Project Logo";
+  }
+
+  @Override
+  protected String getTemplatePath() {
+    return "/projectLogoWidget.erb";
+  }
+
+}
