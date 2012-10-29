02cf9242b98f547d4f1279832dda27d48bf3e682
jbadenas <jbadenas@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-10-29 08:41:37 +0000

    [maven-release-plugin]  copy for tag sonar-tab-metrics-plugin-1.1

---
+++ tags/sonar-tab-metrics-plugin-1.1/src/main/java/org/codehaus/sonar/plugins/tabmetrics/TabMetricsPlugin.java
@@ -0,0 +1,47 @@
+/*
+ * Sonar Tab Metrics Plugin
+ * Copyright (C) 2012 eXcentia
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.codehaus.sonar.plugins.tabmetrics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.sonar.plugins.tabmetrics.resourcetab.GwtTabMetricsResourceTab;
+import org.sonar.api.Extension;
+import org.sonar.api.SonarPlugin;
+
+
+/**
+ * Implements TabMetricsPlugin for Sonar
+ */
+public class TabMetricsPlugin extends SonarPlugin {
+
+  /**
+   * Returns Classes to use into the plugin
+   * 
+   * @return the classes to use into the plugin
+   */
+  public final List<Class<? extends Extension>> getExtensions() {
+    List<Class<? extends Extension>> list = new ArrayList<Class<? extends Extension>>();
+
+    list.add(GwtTabMetricsResourceTab.class);
+
+    return list;
+  }
+}
---
+++ tags/sonar-tab-metrics-plugin-1.1/src/main/java/org/codehaus/sonar/plugins/tabmetrics/resourcetab/GwtTabMetricsResourceTab.java
@@ -0,0 +1,46 @@
+/*
+ * Sonar Tab Metrics Plugin
+ * Copyright (C) 2012 eXcentia
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.codehaus.sonar.plugins.tabmetrics.resourcetab;
+
+import org.sonar.api.web.GwtPage;
+import org.sonar.api.web.NavigationSection;
+import org.sonar.api.web.UserRole;
+
+/**
+ * Gwt TabMetrics Resource Tab
+ */
+@NavigationSection(NavigationSection.RESOURCE_TAB)
+@UserRole(UserRole.USER)
+public class GwtTabMetricsResourceTab extends GwtPage {
+
+  /**
+   * Gets the TabMetrics Tab Title
+   */
+  public final String getTitle() {
+    return "Metrics";
+  }
+
+  /**
+   * Obtains the TabMetrics Tab Id
+   */
+  public final String getGwtId() {
+    return "org.codehaus.sonar.plugins.tabmetrics.resourcetab.TabMetricsPluginTab";
+  }
+}
