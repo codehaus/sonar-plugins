cb2e75ba70f28935b152770ef902051ffaaf892a
ppapapetrou <ppapapetrou@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-11-22 11:57:40 +0000

    Sonar version in pom.xml changed to 3.0 Removed calls to Sonar deprecated API Password property changed to PropertyType.PASSWORD Issue #SONARPLUGINS-2456

--- trunk/google-calendar/src/test/java/org/sonar/plugins/googlecalendar/GoogleCalendarPublisherTest.java
+++ trunk/google-calendar/src/test/java/org/sonar/plugins/googlecalendar/GoogleCalendarPublisherTest.java
@@ -17,64 +17,57 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
-
 package org.sonar.plugins.googlecalendar;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import org.apache.commons.configuration.BaseConfiguration;
 import org.apache.commons.configuration.Configuration;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.sonar.api.config.Settings;
 import org.sonar.api.platform.Server;
 import org.sonar.api.resources.Project;
 
 public class GoogleCalendarPublisherTest {
+
   private transient GoogleCalendarPublisher publisher;
   private transient Project project;
-  private transient Configuration configuration;
+  private transient Settings settings = new Settings();
 
   @Before
   public final void setUp() {
     Server server = mock(Server.class);
     when(server.getURL()).thenReturn("http://localhost:9000");
-    publisher = new GoogleCalendarPublisher(server);
+    settings.setProperty(GoogleCalendarPublisher.CALENDAR_ID_PROP, "123@group.calendar.google.com");
+    settings.setProperty(GoogleCalendarPublisher.ENABLED_PROP, "true");
+    settings.setProperty(GoogleCalendarPublisher.ACCOUNT_PROP, "user@gmail.com");
+    settings.setProperty(GoogleCalendarPublisher.PASSWORD_PROP, "password");
+    publisher = new GoogleCalendarPublisher(server, settings);
     project = new Project("com.ots.ejustice.example:dummy", "", "Dummy");
-    configuration = new BaseConfiguration();
-    configuration.setProperty(
-            GoogleCalendarPublisher.CALENDAR_ID_PROP, "123@group.calendar.google.com");
-    configuration.setProperty(
-            GoogleCalendarPublisher.ENABLED_PROP, "true");
-    configuration.setProperty(
-            GoogleCalendarPublisher.ACCOUNT_PROP, "user@gmail.com");
-    configuration.setProperty(
-            GoogleCalendarPublisher.PASSWORD_PROP, "password");
-    project.setConfiguration(configuration);
   }
 
   @Test
   public final void testGoogleCalendarURL() {
-    assertThat(publisher.getCalendarURL(configuration.
-            getString(GoogleCalendarPublisher.CALENDAR_ID_PROP)),
+    assertThat(publisher.getCalendarURL(settings.getString(GoogleCalendarPublisher.CALENDAR_ID_PROP)),
             is("http://www.google.com/calendar/feeds/123@group.calendar.google.com/private/full"));
   }
 
   @Test
   public final void testEventTitle() {
-        assertThat(publisher.getTitle(project), is("Sonar analysis of Dummy"));
-    }
+    assertThat(publisher.getTitle(project), is("Sonar analysis of Dummy"));
+  }
 
   @Test
   public final void testEventContent() {
-        assertThat(publisher.getContent(project),
-                is("New Sonar analysis of Dummy is available online at http://localhost:9000/project/index/com.ots.ejustice.example:dummy"));
+    assertThat(publisher.getContent(project),
+            is("New Sonar analysis of Dummy is available online at http://localhost:9000/project/index/com.ots.ejustice.example:dummy"));
   }
 
   @Test
   public final void testExecuteOn() {
-      //publisher.executeOn(project, null);
-    }
+    publisher.executeOn(project, null);
+  }
 }
