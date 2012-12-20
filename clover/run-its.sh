3ccbe367d8f7c96e4920327771c18b9ae4bae27b
ogaudin <ogaudin@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-12-20 12:52:04 +0000

    [maven-release-plugin]  copy for tag sonar-emma-plugin-1.3

--- clover/run-its.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn clean verify -Pintegration-tests -Djava.io.tmpdir=/tmp -Dsonar.runtimeVersion=3.0
--- sonargraph/src/test/AlarmClockMain_partialWorkspace2/Foundation/src/main/java/com/hello2morrow/dda/foundation/common/F3.java
+++
@@ -1,7 +0,0 @@
-package com.hello2morrow.dda.foundation.common;
-
-
-public class F3
-{
-    F4 x;
-}
