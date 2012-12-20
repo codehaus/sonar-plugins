6f84f15edef22516a6a77af584f744408518f015
ogaudin <ogaudin@d25a0af3-c461-0410-a42d-853a01480fe4>
2012-12-20 15:36:03 +0000

    [maven-release-plugin]  copy for tag sonar-motion-chart-plugin-1.5

---
+++ tags/sonar-motion-chart-plugin-1.5/src/main/java/org/sonar/plugins/motionchart/WidgetConstants.java
@@ -0,0 +1,39 @@
+/*
+ * Sonar Motion Chart Plugin
+ * Copyright (C) 2009 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.motionchart;
+
+public interface WidgetConstants {
+  String METRIC_X_PROP_KEY = "metric-x";
+  String METRIC_X_PROP_DEF_VALUE = "ncloc";
+
+  String METRIC_Y_PROP_KEY = "metric-y";
+  String METRIC_Y_PROP_DEF_VALUE = "coverage";
+
+  String METRIC_COLOR_PROP_KEY = "metric-color";
+  String METRIC_COLOR_PROP_DEF_VALUE = "duplicated_lines";
+
+  String METRIC_SIZE_PROP_KEY = "metric-size";
+  String METRIC_SIZE_PROP_DEF_VALUE = "violations";
+
+  String CHART_HEIGHT_PROP_KEY = "chartHeight";
+  String CHART_HEIGHT_PROP_DEF_VALUE = "400";
+
+  String INIT_CHART_ON_LATEST_DATE = "initChartOnLatestDate";
+}
---
+++ tags/sonar-motion-chart-plugin-1.5/src/main/resources/static/motionchart-sonar.js
@@ -0,0 +1,45 @@
+function loadMotionChart(widgetId, queryString, initOnLastDate) {
+  $j('#motion-chart-loading-' + widgetId).show();
+  $j('#motion-chart-nodata-' + widgetId).hide();
+
+  var period = $j('#motion-chart-form-' + widgetId).find('select[name="period"]').val();
+  queryString += '&period=' + period;
+
+  var components = $j('#motion-chart-form-' + widgetId).find('input[name="components"]:checked');
+  if (components.length>0) {
+    queryString += '&components=true';
+  }
+  var query = new google.visualization.Query(baseUrl + '/motion_chart?' + queryString);
+  query.send(function (response) {
+    $j('#motion-chart-loading-' + widgetId).hide();
+    if (response.isError()) {
+      error(response.getDetailedMessage());
+      return;
+    }
+    if (response.getDataTable().getNumberOfRows() > 0) {
+      $j('#motion-chart-' + widgetId).show();
+      drawMotionChart(response.getDataTable(), widgetId, initOnLastDate);
+    } else {
+      $j('#motion-chart-' + widgetId).hide();
+      $j('#motion-chart-nodata-' + widgetId).show();
+    }
+  });
+}
+
+function drawMotionChart(data_table, widgetId, initOnLastDate) {
+  var chart = new google.visualization.MotionChart(document.getElementById('motion-chart-' + widgetId));
+  var options = {};
+  if (initOnLastDate) {
+    var latestDate = data_table.getDistinctValues(1).last();
+    var year = latestDate.getFullYear();
+    var month = latestDate.getMonth() + 1;
+    if (month < 10) {
+      month = "0" + month;
+    }
+    var day = latestDate.getDate();
+    options['state'] = '{"time": "' + year + '-' + month + '-' + day + '"}';
+  }
+  options['width'] = $j('#motion-chart-' + widgetId).width() - 10;
+  options['height'] = $j('#motion-chart-' + widgetId).height() - 10;
+  chart.draw(data_table, options);
+}
